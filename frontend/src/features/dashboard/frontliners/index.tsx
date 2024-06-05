1a7067103d3653cf8e4c834ae17b83a308c86575
kriptonhaz <kriptonhaz@gmail.com>
2024-06-05 21:20:32 +0700

    rename import frontliners

--- frontend/src/features/dashboard/frontliners/index.tsx
+++ frontend/src/features/dashboard/frontliners/index.tsx
@@ -1,12 +1,12 @@
 import DashboardLayout from '@/layouts/dashboard.layout';
 import * as React from 'react';
-import ListVolunteer from './ui/ListVolunteer';
+import ListFrontliners from './ui/ListFrontliners';
 
 const FrontlinersUI: React.FC = () => {
   return (
     <DashboardLayout title='Frontliners' subtitle='Honoring frontliners for going extra mile'>
       <DashboardLayout.Content>
-        <ListVolunteer />
+        <ListFrontliners />
       </DashboardLayout.Content>
       {/* <DashboardLayout.Activity>
       </DashboardLayout.Activity> */}
