4c0b4a37f7d4bd3860ce8953e7d142d7d3192a06
kriptonhaz <kriptonhaz@gmail.com>
2024-07-23 17:18:33 +0700

    clean code

--- frontend/src/hooks/use-frontliners.hook.tsx
+++ frontend/src/hooks/use-frontliners.hook.tsx
@@ -11,7 +11,6 @@ import {
   FrontlinerAddResponseType,
   IAddSingleFrontlineResponse,
   IFormSingleAddFrontliner,
-  IFrontliner,
   IFrontlinerDeleteResponse,
 } from '@/interface/frontliners.interface';
 
